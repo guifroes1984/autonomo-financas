4a0050b072bd13055b2ef682e320b41345569729
guifroes1984 <guilhermefroes26@gmail.com>
2026-07-27 00:06:47 -0300

    chore: configure Spring Boot backend

---
+++ backend/src/main/java/com/autonomofinancas/AutonomoFinancasApplication.java
@@ -0,0 +1,13 @@
+package com.autonomofinancas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutonomoFinancasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutonomoFinancasApplication.class, args);
+	}
+
+}
